26731dcc38a32df4fd164117309624f3ff3c106b
grdavis <grdavis@mit.edu>
2021-10-21 19:21:30 -0400

    update scraper and requirements

--- scraper.py
+++ scraper.py
@@ -1,35 +1,19 @@
 import datetime
-import csv
-from selenium import webdriver
 from bs4 import BeautifulSoup
 import utils
+import requests
 
 URL = 'https://www.sports-reference.com/cbb/boxscores/index.cgi?month=MONTH&day=DAY&year=YEAR'
 DATA_FOLDER = utils.DATA_FOLDER
 
-def new_driver():
-	'''
-	return a new healdless chrome webdriver
-	'''
-	chrome_options = webdriver.ChromeOptions()  
-	chrome_options.add_argument("--headless")
-	return webdriver.Chrome(options = chrome_options)
-
-def get_chrome_data(url, driver):
-	'''
-	given a driver object, retrieve the page source of the specified url
-	'''
-	driver.get(url)
-	return driver.page_source
-
-def scrape_scores(date_obj, driver):
+def scrape_scores(date_obj):
 	'''
 	scrape and return stats in the form of a list of lists where each sublist is information from a single game played on the specified day 
 	'''
 	day, month, year = str(date_obj.day), str(date_obj.month), str(date_obj.year)
 	day_stats = []
 	url = URL.replace("DAY", day).replace("MONTH", month).replace("YEAR", year)
-	data = get_chrome_data(url, driver)
+	data = requests.get(url).content
 	table_divs = BeautifulSoup(data,'html.parser').find_all("div", {'class': 'game_summary'})
 	this_day_string = date_obj.strftime('%Y%m%d')
 	print(this_day_string)
@@ -52,7 +36,6 @@ def scrape_by_day(file_start, scrape_start, scrape_end, all_data):
 	scrape_end: this is the date object specifying what the last day to scrape should be
 	all_data: this is a list of all the existing data that the new data will be appended to
 	'''
-	driver = new_driver()
 	new_data = []
 	i = scrape_start
 	this_month = scrape_start.month
@@ -60,7 +43,7 @@ def scrape_by_day(file_start, scrape_start, scrape_end, all_data):
 		if i.month in [5, 6, 7, 8, 9, 10]: 
 			i += datetime.timedelta(days = 1)
 			continue
-		new_data.extend(scrape_scores(i, driver))
+		new_data.extend(scrape_scores(i))
 		i += datetime.timedelta(days = 1)
 		if i.month != this_month:
 			this_month = i.month
@@ -71,7 +54,6 @@ def scrape_by_day(file_start, scrape_start, scrape_end, all_data):
 	print(len(new_data), "games recorded")
 	all_data.extend(new_data)
 	utils.save_data(DATA_FOLDER + file_start + "-" + (i - datetime.timedelta(days = 1)).strftime('%Y%m%d') + ".csv", all_data)
-	driver.quit()
 	return all_data
 
 def main(file_start, scrape_start, scrape_end, data_filepath = False):
