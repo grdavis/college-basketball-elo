e7276cfc11edae62eba76d5d76701d3f1c68359e
grdavis <grdavis@mit.edu>
2022-03-13 19:20:37 -0400

    fix typo

--- utils.py
+++ utils.py
@@ -59,7 +59,7 @@ def clean_up_old_outputs_and_data():
 	remove_files([SPREAD_FOLDER + f for f in sorted_files], 3)
 
 	#OUTPUTS
-	r = re.compile(".*Predictions.*.csv")
+	r = re.compile(".*Game Predictions.*.csv")
 	elligible_data = list(filter(r.match, os.listdir(OUTPUTS_FOLDER)))
 	sorted_files = sorted(elligible_data, key = lambda x: x[:8], reverse = True)
 	remove_files([OUTPUTS_FOLDER + f for f in sorted_files], 3)
