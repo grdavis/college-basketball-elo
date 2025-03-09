48608d5681fa27e605701f1f23aa082612447938
grdavis <grdavis@mit.edu>
2025-03-09 11:03:45 -0700

    fix historical data and update conference tournaments

--- Data/health_check.py
+++ Data/health_check.py
@@ -1,7 +1,10 @@
 import pandas as pd
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import utils
 
-df = pd.read_csv(utils.get_latest_data_filepath().strip('Data/'), 
+df = pd.read_csv(utils.get_latest_data_filepath(), 
                 header=None, 
                 names = ['neutral', 'away', 'ascore', 'home', 'hscore', 'date', 'aspread'])
 
@@ -11,27 +14,50 @@ home_teams = df[['home', 'date']].rename(columns={'home': 'team'})
 all_teams = pd.concat([away_teams, home_teams])
 
 def check_1():
-    # Find all the team names in our data log and finds their first and last appearance dates
-    # Group by team and calculate first date, last date, and count
+    '''
+    This function finds the first and last appearance dates of each team and the total number of appearances to 
+    help validate that team names aren't changing mid-season
+    '''
     team_stats = all_teams.groupby('team').agg({
         'date': ['min', 'max', 'count']
     }).reset_index()
-
-    # Rename columns
     team_stats.columns = ['team', 'first_appearance', 'last_appearance', 'total_appearances']
-
-    # Output team stats to CSV
     team_stats.to_csv('Data/team_stats_health_check_1.csv', index=False)
 
 def check_2():
-    # Count games since start of 2024-25 season
+    '''
+    This function checks the number of games each team has played since an inputed date to help validate 
+    that team names aren't changing mid-season
+    '''
     recent_games = all_teams[all_teams['date'] > 20240801].groupby('team').size().reset_index(name='games_since_20240801')
-
-    # Sort by number of games descending and get top 100
     recent_games_sorted = recent_games.sort_values('games_since_20240801', ascending=False)
-
-    # Output to CSV
     recent_games_sorted.to_csv('Data/team_stats_health_check_2.csv', index=False)
+
+def check_3():
+    '''
+    This function checks for duplicate games by looking for repeat combinations of away, away_score, home, home_score
+    '''
+    potential_dupes = df.groupby(['away', 'ascore', 'home', 'hscore']).agg(
+        count=('date', 'size'),
+        min_date=('date', 'min'),
+        max_date=('date', 'max')
+    ).reset_index()
+    potential_dupes = potential_dupes[potential_dupes['count'] > 1]
+    
+    # Extract year from min_date and max_date
+    potential_dupes['min_year'] = potential_dupes['min_date'].astype(str).str[:4]
+    potential_dupes['max_year'] = potential_dupes['max_date'].astype(str).str[:4]
+
+    # Filter to rows where min_year equals max_year as that makes duplicate more likely
+    potential_dupes = potential_dupes[potential_dupes['min_year'] == potential_dupes['max_year']]
+    potential_dupes = potential_dupes.drop(['min_year', 'max_year'], axis=1)
+
+    if len(potential_dupes) > 0:
+        print(f"Found {len(potential_dupes)} potential duplicates:")
+        print(potential_dupes)
+    else:
+        print("No duplicate games found")
 
 # check_1()
 # check_2()
+# check_3() # last checked 2025-03-09; games before that date have been validated as non-duplicates
