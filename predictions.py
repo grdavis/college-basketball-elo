2240ca9e5d9ba4f13a48cd1676c0e7152ae85805
grdavis <grdavis@mit.edu>
2025-03-15 07:25:36 -0700

    fix neutral flags and tournament predictions

--- predictions.py
+++ predictions.py
@@ -122,7 +122,10 @@ def predict_next_day(elo_state, forecast_date, auto):
 		return
 	predictions = []
 	for game in games:
-		is_neutral = True if game[0] == 1 else False
+		if forecast_date.strftime('%Y%m%d')[4:] >= '0314': #force neutral site for games after 3/14 because they should all be conference tournament games
+			is_neutral = 1
+		else:
+			is_neutral = True if game[0] == 1 else False
 		winner, prob, home_spread = predict_game(elo_state, game[3], game[1], pick_mode = 1, neutral = is_neutral)
 		if game[1] == winner:
 			predictions.append([game[0], game[1], prob, -home_spread, game[3], "{0:.0%}".format(1 - (float(prob[:-1])/100)), home_spread])
